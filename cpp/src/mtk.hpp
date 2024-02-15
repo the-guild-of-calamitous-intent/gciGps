8811ff620fa1bb7c91f7472521d5d44abeee2f5d
walchko <walchko@users.noreply.github.com>
2024-02-14 21:02:44 -0700

    update

--- cpp/src/mtk.hpp
+++ cpp/src/mtk.hpp
@@ -40,33 +40,33 @@
 // $PMTK001,220,3*30 -> (220) set pos fix, GCI_UPDATE_1HZ
 /////////////////////////////////////////////////////////////////////////////
 
-// PMTK commands
-// https://www.sparkfun.com/datasheets/GPS/Modules/PMTK_Protocol.pdf
-constexpr uint8_t GCI_UPDATE_1HZ[] = "$PMTK220,1000*1F\r\n"; //  1 Hz
-constexpr uint8_t GCI_UPDATE_2HZ[] = "$PMTK220,500*2B\r\n";  //  2 Hz
-// constexpr uint8_t GCI_UPDATE_5HZ[] = "$PMTK220,200*2C\r\n";  //  5 Hz ... invalid?
-// constexpr uint8_t GCI_UPDATE_10HZ[] = "$PMTK220,100*2F\r\n"; // 10 Hz ... invalid? must > 200, only 100
+// // PMTK commands
+// // https://www.sparkfun.com/datasheets/GPS/Modules/PMTK_Protocol.pdf
+// constexpr uint8_t GCI_UPDATE_1HZ[] = "$PMTK220,1000*1F\r\n"; //  1 Hz
+// constexpr uint8_t GCI_UPDATE_2HZ[] = "$PMTK220,500*2B\r\n";  //  2 Hz
+// // constexpr uint8_t GCI_UPDATE_5HZ[] = "$PMTK220,200*2C\r\n";  //  5 Hz ... invalid?
+// // constexpr uint8_t GCI_UPDATE_10HZ[] = "$PMTK220,100*2F\r\n"; // 10 Hz ... invalid? must > 200, only 100
 
-// Position fix update rates
-// Can't fix position faster than 5 times a second!
-constexpr uint8_t GCI_FIX_CTL_1HZ[] = "$PMTK300,1000,0,0,0,0*1C\r\n"; // 1 Hz output
-constexpr uint8_t GCI_FIX_CTL_5HZ[] = "$PMTK300,200,0,0,0,0*2F\r\n";  // 5 Hz output
+// // Position fix update rates
+// // Can't fix position faster than 5 times a second!
+// constexpr uint8_t GCI_FIX_CTL_1HZ[] = "$PMTK300,1000,0,0,0,0*1C\r\n"; // 1 Hz output
+// constexpr uint8_t GCI_FIX_CTL_5HZ[] = "$PMTK300,200,0,0,0,0*2F\r\n";  // 5 Hz output
 
-constexpr uint8_t GCI_BAUD_115200[] = "$PMTK251,115200*1F\r\n"; // 115200 bps
-constexpr uint8_t GCI_BAUD_57600[] = "$PMTK251,57600*2C\r\n";   //  57600 bps
-constexpr uint8_t GCI_BAUD_9600[] = "$PMTK251,9600*17\r\n";     //   9600 bps
+// constexpr uint8_t GCI_BAUD_115200[] = "$PMTK251,115200*1F\r\n"; // 115200 bps
+// constexpr uint8_t GCI_BAUD_57600[] = "$PMTK251,57600*2C\r\n";   //  57600 bps
+// constexpr uint8_t GCI_BAUD_9600[] = "$PMTK251,9600*17\r\n";     //   9600 bps
 
-constexpr uint8_t GCI_ANTENNA[] = "$PGCMD,33,1*6C\r\n"; // request for updates on antenna status
-constexpr uint8_t GCI_NOANTENNA[] = "$PGCMD,33,0*6D\r\n"; // don't show antenna status messages
+// constexpr uint8_t GCI_ANTENNA[] = "$PGCMD,33,1*6C\r\n"; // request for updates on antenna status
+// constexpr uint8_t GCI_NOANTENNA[] = "$PGCMD,33,0*6D\r\n"; // don't show antenna status messages
 
-constexpr uint8_t GCI_ENABLE_SBAS[] = "$PMTK313,1*2E\r\n"; // Enable search for SBAS satellite (only works with 1Hz < output rate)
-constexpr uint8_t  GCI_ENABLE_WAAS[] = "$PMTK301,2*2E\r\n"; // WAAS for DGPS correction data
+// constexpr uint8_t GCI_ENABLE_SBAS[] = "$PMTK313,1*2E\r\n"; // Enable search for SBAS satellite (only works with 1Hz < output rate)
+// constexpr uint8_t  GCI_ENABLE_WAAS[] = "$PMTK301,2*2E\r\n"; // WAAS for DGPS correction data
 
-constexpr uint8_t GCI_GLL[] = "$PMTK314,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n"; // only the GLL sentence
-constexpr uint8_t GCI_RMC[] = "$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n"; // only the RMC sentence
-// constexpr uint8_t GCI_VTG[] = "$PMTK314,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29" // only the VTG
-constexpr uint8_t GCI_GGA[] = "$PMTK314,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n"; // just the GGA
-constexpr uint8_t GCI_GSA[] = "$PMTK314,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n"; // just the GSA
-// constexpr uint8_t GCI_GSV[] = "$PMTK314,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0*29" // just the GSV
-constexpr uint8_t GCI_RMCGGA[] = "$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28\r\n"; // RMC and GGA
-constexpr uint8_t GCI_RMCGGAGSA[] = "$PMTK314,0,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n"; // RMC, GGA and GSA
+// constexpr uint8_t GCI_GLL[] = "$PMTK314,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n"; // only the GLL sentence
+// constexpr uint8_t GCI_RMC[] = "$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n"; // only the RMC sentence
+// // constexpr uint8_t GCI_VTG[] = "$PMTK314,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29" // only the VTG
+// constexpr uint8_t GCI_GGA[] = "$PMTK314,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n"; // just the GGA
+// constexpr uint8_t GCI_GSA[] = "$PMTK314,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n"; // just the GSA
+// // constexpr uint8_t GCI_GSV[] = "$PMTK314,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0*29" // just the GSV
+// constexpr uint8_t GCI_RMCGGA[] = "$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28\r\n"; // RMC and GGA
+// constexpr uint8_t GCI_RMCGGAGSA[] = "$PMTK314,0,1,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n"; // RMC, GGA and GSA
